94e07a8e9b4af096909706598c397ff4667b453d
Dóri <k.dorina5@gmail.com>
2017-11-23 14:00:52 +0100

    bootstrap

--- cinema_world/cinema-outer-world/src/app/app.module.ts
+++ cinema_world/cinema-outer-world/src/app/app.module.ts
@@ -1,14 +1,16 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
